c1328de2070ce8f462284268faf7e74a35352696
rinathiLM <rina.thi@libertymutual.com>
2017-09-13 23:05:32 -0400

    pushing unit 9 work again for practice

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards [1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log ("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
-  if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
   } else {
-    alert("Sorry, try again.");
+  console.log("Sorry, try again.");
   }
 };
+
+var flipCard = function(cardId){
+  console.log("user flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+  };
+
+flipCard(0);
+flipCard(1);
 
 //var cards = ["queen", "queen", "king", "king"];
 //var cardsInPlay= [];
