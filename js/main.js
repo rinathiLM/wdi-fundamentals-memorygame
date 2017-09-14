917732ab160c122552fda9e91c55334ceae0f132
rinathiLM <rina.thi@libertymutual.com>
2017-09-14 00:08:18 -0400

    practicing unit 10 prework

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
   } else {
   console.log("Sorry, try again.");
   }
+}
 };
 
 var flipCard = function(cardId){
-  console.log("user flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("user flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
   };
 
